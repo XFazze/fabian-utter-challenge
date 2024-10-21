ef2e891b69b8aa37b7c023ea4f113daeb8ceac2b
xfazze <blixtvinge@gmail.com>
2024-10-21 17:00:27 +0200

    Added people

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
+import datetime
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from os import getenv
 from requests import get
-
 load_dotenv()
 
 # Initializing flask
@@ -10,8 +10,11 @@ app = Flask(__name__, static_folder="static")
 app.config["SECRET_KEY"] = getenv("secret-key")
 
 
-def create_user(summonerId: str, name: str, riot_name: str, riot_tag: str):
-    api_url = f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}?api_key={getenv('riot-token')}"
+def create_user(
+    summonerId: str, name: str, riot_name: str, riot_tag: str, server: str = "euw1"
+):
+
+    api_url = f"https://{server}.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}?api_key={getenv('riot-token')}"
 
     rank_api = get(api_url)
     # app.logger.debug(api_url)
@@ -44,15 +47,48 @@ def create_user(summonerId: str, name: str, riot_name: str, riot_tag: str):
 
 @app.route("/")
 def index():
-    fabian_summonerId = "Sg9XI880vfFrXYfpBJaq2PxAeaoOaLO3sJh69NGPHv4JgjteqSRMNHeXGw"
-    utter_summonerId = "CjlfRa9w4LMXs2HFaJmZN7z52-PSxlVd2eU13zTUt9jwpgVu"
-    #utter_summonerId = "_tj833H4mvDeiajG95DpYoINt1jg0IXZBqQYmN6YAfBjFlYnTNujiRPtLA"
-
+    summoner_id = {
+        "fabian": "Sg9XI880vfFrXYfpBJaq2PxAeaoOaLO3sJh69NGPHv4JgjteqSRMNHeXGw",
+        "utter": "CjlfRa9w4LMXs2HFaJmZN7z52-PSxlVd2eU13zTUt9jwpgVu",
+        "noel": "z8OdNeGyWZ9ygC6OguFNsizZaSGhgxdg1giLqJeGkPWHPLvgDQhU20r5Dg",
+        "olof": "8gv_pVLgDvmEGfLBnJUE554z3REvEkknBoTpqhe9wcVtmZhX",
+        "ivorn": "0uoMgTHxBamOKxLCe8vWmwGxdyCmfGjYwZYJcRSyWmEwlbVkaxK6rB_N8A",
+        "chris": "5tV5tzsUI9iako8U3OwGMbAXgfqwiBr9GvoaI3aCa0-zHEEX",
+        "nix": "AgN3PBU6E98Fl6Gw2vEPQx5Z6uirglao2lcaX-HAlFPEMtHu",
+        "octo": "CFMnuiVGdtzmdeWkJK-fJRshV3II2FUXWamrHR-Sl6eEYcHU",
+        "abbe": "rGhM39SwatdCI8MMlwlFNQJ3GGPnCPNxqq9WMlaIzdw4OlI",
+        "leo": "nmbjtla69-VM-0IVtbgJ6skxP6PDQZa_3imnDmNUfC9G-S0",
+        "alex": "k7woLZsse5G2yMMecun2gtr00A5F0mhr8QCEdy-qO_WGTiE",
+        "emser": "PSqliQsYNxzU5mEBVlgMKI4F5RGVgb1ls9Bs2kfw6K0L27s",
+        "johanna": "lammjKiWJ_0BmUyLPlACs-ZXBH8VkjkB0FjuJE7y9kU1f5E",
+    }
     return render_template(
         "index.html",
-        players=[
-            create_user(fabian_summonerId, "Fabian", "HAHAHA", "EUNEE"),
-            create_user(utter_summonerId, "Utter", "utter THE butter", "EUW"),
+        top_players=[
+            create_user(summoner_id["fabian"], "Fabian", "HAHAHA", "EUNEE"),
+            create_user(summoner_id["noel"], "Utter", "utter THE butter", "EUW"),
+        ],
+        challenge_time = datetime.date(year=2024, month=11, day=4)
+        all_players=[
+            create_user(summoner_id["noel"], "Noel", "im alright", "0000"),
+            create_user(summoner_id["olof"], "Olof", "vG0Dv", "EUW"),
+            create_user(summoner_id["ivorn"], "Ivorn", "ENJOYABLEGAME123", "SPIR"),
+            create_user(summoner_id["chris"], "Chris", "twice is mommy", "ff15"),
+            create_user(summoner_id["nix"], "Rasmus", "Ñix", "EUW"),
+            create_user(summoner_id["octo"], "Simon", "mimass", "EUW"),
+            create_user(
+                summoner_id["abbe"], "Abbe", "Trockeltum", "0003", server="eun1"
+            ),
+            create_user(summoner_id["leo"], "Leo", "Leo", "12333", server="eun1"),
+            create_user(
+                summoner_id["alex"], "Alex", "godslayerenryu", "EUNE", server="eun1"
+            ),
+            create_user(
+                summoner_id["emser"], "Emelie", "notemser10", "8567", server="eun1"
+            ),
+            create_user(
+                summoner_id["johanna"], "Johanna", "jechir", "EUNE", server="eun1"
+            ),
         ],
     )
 
---
+++ src/static/js/timer.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  timer();
+};
+
+function timer() {
+  var time = document.getElementById("timer")?.innerHTML;
+  time;
+  setTimeout(timer, 1000);
+}
